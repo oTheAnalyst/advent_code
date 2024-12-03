bc290e2474d1ff6ff6ccb36d283edbebd5f99542
VtheRtech <michaelvcumbo21@proton.me>
2024-12-03 12:57:06 -0500

    new data & script

--- day1.R
+++ day1.R
@@ -2,9 +2,22 @@ library(tidyverse)
 library(rstudioapi)
 library(clipr)
 
-# data reads 
-# day1 <- clipr::read_clip_tbl()
-# base::save(day1, file = "day1_data.rda")
+ data reads 
+day1 <- clipr::read_clip()
+ base::save(day1, file = "day1_data.rda")
 
 base::load(file = "day1_data.rda")
 head(day1)
+
+
+colnames(day1)
+ str(day1)
+
+data <- str_split(data, pattern = "   ")
+
+library(stringr)
+data <- str_split(data, pattern = "   ")
+data <- data.frame(t(data.frame(data)))
+data <- data.frame(cbind(sort(data$X1, decreasing = TRUE), sort(data$X2, decreasing = TRUE)))
+data$diff <- abs(as.numeric(data[,1]) - as.numeric(data[,2]))
+sum(data$diff))
