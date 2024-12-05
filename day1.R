0f078d526b882536faaad486d35a5fdbd48fdd23
oTheAnalyst <michaelvcumbo21@proton.me>
2024-12-05 09:12:39 -0500

    day 1 update

--- day1.R
+++ day1.R
@@ -1,23 +1,54 @@
 library(tidyverse)
-library(rstudioapi)
+library(stringr)
 library(clipr)
+library(purrr)
+library(dplyr)
 
- data reads 
-day1 <- clipr::read_clip()
- base::save(day1, file = "day1_data.rda")
+dplyr::col_bind
+
+# day1 <- clipr::read_clip()
+# base::save(day1, file = "day1_data.rda")
+
+# load data
+base::load(file = "day1_data.rda")
+
+# part 1
+day1 <- stringr::str_split(day1, pattern = "  ")
+day1 <- data.frame(base::t(base::data.frame(day1)))
+day1 <- data.frame(cbind(sort(day1$X1, decreasing = TRUE), sort(day1$X2, decreasing = TRUE)))
+day1$diff <- abs(as.numeric(day1[,1]) - as.numeric(day1[,2]))
+sum(day1$diff)
+str(day1)
 
 base::load(file = "day1_data.rda")
-head(day1)
+
+# part 2 
+day1 <- stringr::str_split(day1, pattern = "   ")
+day1 <- data.frame(t(data.frame(data)))
+query <- as.numeric(day1$X1)
+table <- data.frame(table(as.numeric(day1$X2)))
+
+total = 0
+for (i in 1:length(query)){
+  number = query[i]
+  if (nrow(table[table$Var1 == number,]) > 0){
+    total <- total + (number * table[table$Var1 == number,]$Freq)
+  }
+}
 
 
-colnames(day1)
- str(day1)
 
-data <- str_split(data, pattern = "   ")
 
-library(stringr)
-data <- str_split(data, pattern = "   ")
-data <- data.frame(t(data.frame(data)))
-data <- data.frame(cbind(sort(data$X1, decreasing = TRUE), sort(data$X2, decreasing = TRUE)))
-data$diff <- abs(as.numeric(data[,1]) - as.numeric(data[,2]))
-sum(data$diff))
+calculate_total <- function(query_value, table) {
+  number <- query_value
+  if (nrow(table[table$Var1 == number, ]) > 0) {
+    return(number * table[table$Var1 == number,]$Freq)
+  } else {
+    return(0)  # Return 0 if the condition is not met
+  }
+}
+
+total_results <- map_dbl(query, ~calculate_total(.x, table))
+total <- sum(total_results)
+print(total)
+
